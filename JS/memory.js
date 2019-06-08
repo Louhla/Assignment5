82a8817f537d9095a08f79fef7cfcf926d7035dd
Lola <lolapaulinebach@gmail.com>
2019-06-08 13:19:52 +0300

    added new game functionality and count wrong guesses, optimized disableing clicks while two cards are open by remoing click event

--- JS/memory.js
+++ JS/memory.js
@@ -1,10 +1,11 @@
 var cardsArray = ["img/jungle1.jpg", "img/jungle1.jpg", "img/jungle2.jpg", "img/jungle2.jpg", "img/jungle3.jpg", "img/jungle3.jpg", "img/jungle4.jpg", "img/jungle4.jpg", "img/jungle5.jpg", "img/jungle5.jpg", "img/jungle6.jpg", "img/jungle6.jpg"]
 var openCards = [];
 var solvedCards = 0;
+var wrongGuesses = 0;
 var array = shuffle(cardsArray);
 
 //Find Matching Cards
-$('.cards').click(function (e) {
+$('.cards').on("click", function clickCard(e) {
     if (openCards.length < 2) {
         var index = e.target.id;
         clickedCard = $(this);
@@ -13,31 +14,32 @@ $('.cards').click(function (e) {
         clickedCard.addClass("selected");
     }
     if (openCards.length === 2) {
-        // for (var i = 1; i < openCards.length; i++) {
-        //     for (var j = 0; j < openCards.length; j++) {
-                if (openCards[0] === openCards[1]) {
-                    setTimeout(function(){$(".cards.selected").removeClass("selected").addClass("solved")}, 1000);
-                    setTimeout(function(){openCards.length = 0}, 1000);
-                    solvedCards += 1; 
-                    if(solvedCards === 6){
-                            $('#myModal').modal('show');
-                            $("body").css("background-image", "url('./img/jungle-bg.jpg')");
-                          }
-                }else {
-                    setTimeout(function () { $(".cards.selected").css("background-image", "url('./img/house-small.jpg')"); }, 1000);
-                    setTimeout(function(){openCards.length = 0}, 1000);
-                }
-        //     }
-        // }
-    
+        $(".cards").off('click'); //Remove click event for the time two cards are open
+        setTimeout(function () { $(".cards").on("click", clickCard) }, 1000);
+        if (openCards[0] === openCards[1]) {
+            setTimeout(function () { $(".cards.selected").removeClass("selected").addClass("solved") }, 1000);
+            setTimeout(function () { openCards.length = 0 }, 1000);
+            solvedCards += 1;
+            if (solvedCards === 6) {
+                setTimeout(function () { $("#myModal").modal("show") }, 1000);
+                setTimeout(function () { $("body").css("background-image", "url('./img/flowers-bg.jpg')") }, 1000);
+                $("#guess-result").text("Your amount of wrong guesses: " + wrongGuesses);
+            }
+        } else {
+            wrongGuesses += 1;
+            setTimeout(function () { $(".cards.selected").css("background-image", "url('./img/house-small.jpg')"); }, 1000);
+            setTimeout(function () { openCards.length = 0 }, 1000);
+        }
     }
 });
 
 //New Game
-$("#newGame").on("click", function refresh(){
+$("#newGame").on("click", function refresh() {
     window.location.reload();
 });
-
+$(".modal-button").on("click", function refresh() {
+    window.location.reload();
+});
 
 //Randomize Cards
 function shuffle(array) {
